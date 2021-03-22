3e518271d49f90886ce65690fed6c91a2cacb482
Laurie Waller <lauriewaller7@gmail.com>
2021-03-21 18:20:49 -0700

    correctly pull .image data

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import $ from 'jquery';
 
 function getElements(response) {
   if (response[0].identifier) {
-    $('.showImage').text(`${response[0].identifier}`);
+    $('.showImage').text(`${response[0].image}`);
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
   }
