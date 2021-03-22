a1519a0d011e76d4f33a656b9207a133a54c918d
Laurie Waller <lauriewaller7@gmail.com>
2021-03-21 21:57:21 -0700

    create functions to display mars and apod images

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-import ApodImage from './js/apod-service.js';
-import MarsImage from './js/mars-service.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+import ApodImage from './js/apod-service.js';
+import MarsImage from './js/mars-service.js';
 
-function getElements(response) {
-  if (response.photos[0].img_src) {
-    $('.showImage').html(`<img src="${response.photos[0].img_src}">`);
-  } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
-  }
+// function getElements(response) {
+//   if (response.photos[0].img_src) {
+//     $('.showImage').html(`<img src="${response.photos[0].img_src}">`);
+//   } else {
+//     $('.showErrors').text(`There was an error: ${response.message}`);
+//   }
+// }
+
+function displayApodImage(response) {
+  let url = response.hdurl
+  $('.apodImage').html(`<img src="${url}">`);
+}
+
+function displayMarsImage(response) {
+  let url = response.photos[0].img_src
+  $('.marsImage').html(`<img src="${url}">`);
 }
 
 $("#mars-button").click(function(event) {
